8ad1a2deed992ee46507512dc970c47197907f39
Amol D <dev.amold@gmail.com>
2015-06-26 23:31:05 +0530

    Added fb link

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,10 +5,6 @@ class ApplicationController < ActionController::Base
 	# before_action :authenticate_user!
 	protect_from_forgery with: :exception
 
-	# def new_session_path(scope)
-	# 	new_user_session_path
-	# end
-
 	def redirect_user
 	  redirect_to '/404'
 	end	
--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -1,16 +1,12 @@
 class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
-    skip_before_action :verify_authenticity_token
     def facebook
         @user = User.from_omniauth(request.env["omniauth.auth"])
         if @user.persisted?
-             sign_in_and_redirect @user, :event => :authentication
-            # sign_in @user
-            # redirect_to edit_user_registration_path
+            sign_in_and_redirect @user, :event => :authentication
             set_flash_message(:notice, :success, :kind => "Facebook") if is_navigational_format?
         else
-            Rails.logger.info "----@user-------------------------#{@user.inspect}---"
             session["devise.facebook_data"] = request.env["omniauth.auth"]  #facebook session
-            flash[:notice] = "Email required for facebook login"
+            flash[:notice] = "Email required for login with facebook"
             redirect_to new_user_registration_path
         end
     end
